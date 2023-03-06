dfa6e1f7399364e636ea5cc634942040d2cbd5c9
luquilo <luquilo.etc@gmail.com>
2023-03-06 15:05:07 +0700

    belajar membuat file menjadi lebih modular

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
 void main() => runApp(MaterialApp(
       home: QuoteList(),
@@ -12,36 +13,10 @@ class QuoteList extends StatefulWidget {
 
 class _QuoteListState extends State<QuoteList> {
   List<Quote> quotes = [
-    Quote(text: 'Alhamdulillah', author: 'muhammad'),
+    Quote(text: 'Alhamdulillah', author: 'MUHAMMAD'),
     Quote(text: 'Allahuakbar', author: 'luqmanul'),
     Quote(text: 'MasyaAllah', author: 'hakim'),
   ];
-
-  Widget quoteTemplate(quote) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Text(
-                quote.text,
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.grey[600],
-                ),
-              ),
-              SizedBox(height: 6),
-              Text(quote.author,
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Colors.grey[800],
-                  ))
-            ]),
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +28,7 @@ class _QuoteListState extends State<QuoteList> {
           backgroundColor: Colors.redAccent,
         ),
         body: Column(
-          children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+          children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
         ));
   }
 }
